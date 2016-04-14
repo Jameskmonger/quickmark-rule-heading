aeeb85c3b06bdcd984b4ee202f3ac46fdff7e0d3
James Monger <jameskmonger@hotmail.co.uk>
2016-04-15 00:22:42 +0100

    Implement

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 'use strict';
 
-module.exports = function (input) {
+var REGEXP = /^(#{1,6}) *([\s\S]+?)(?:\n|\\n|$)/;
 
+module.exports = function (input) {
+    var parsed = REGEXP.exec(input);
+
+    if (parsed) {
+        return {
+            original: parsed[0],
+            parsed: parsed[2],
+            depth: parsed[1].length
+        };
+    }
+
+    return undefined;
 };
